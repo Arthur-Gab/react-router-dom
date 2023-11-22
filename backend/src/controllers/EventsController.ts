0a7f476fcd33620b6503efe8b9a9d49af55924a3
Arthur-Gab <arthurgabriel933@gmail.com>
2023-11-22 19:30:30 -0300

    feat(get events): criado a rota na api para recuperação de todos os eventos cadastrados no db

--- backend/src/controllers/EventsController.ts
+++ backend/src/controllers/EventsController.ts
@@ -3,6 +3,14 @@ import { Request, Response } from 'express';
 
 // 🚧 Work on progress 🚧
 
+// Get all events on db
+export async function index(_: Request, res: Response) {
+	const all_events = await prisma.event.findMany();
+
+	return res.status(200).json(all_events);
+}
+
+// Create a Event on db
 export async function store(req: Request, res: Response) {
 	const { title, description, image, date } = req.body;
 
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,4 +4,5 @@ import * as eventsController from './controllers/EventsController';
 
 export const routes = Router();
 
+routes.get('/events', eventsController.index);
 routes.post('/events/create', eventsController.store);
